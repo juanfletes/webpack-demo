08a427ab7142e3a18f155c51d845cee858f2bcc5
Juan Fletes <juanfletes007@gmail.com>
2019-07-22 11:07:03 -0600

    Prevent Duplication

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    another: './src/another-module.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -16,5 +16,10 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
